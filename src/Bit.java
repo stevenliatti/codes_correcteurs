85fb1c66303513e886c25026b024e3d7582d78b7
steven.liatti <steven.liatti@etu.hesge.ch>
2017-05-25 17:28:11 +0200

    Méthode pour construire la matrice G faite
    Modifications qui seront validées :
    	modifié :         src/Bit.java
    	modifié :         src/Main.java
    	modifié :         src/ReedMuller.java

--- src/Bit.java
+++ src/Bit.java
@@ -2,36 +2,44 @@
  * Created by stevenliatti on 25.05.17.
  */
 public class Bit {
-    private byte value;
+    private int value;
 
-    public Bit(byte value) {
+    public Bit(int value) {
         if (value > 1 || value < 0) {
             throw new IllegalArgumentException("The value must be 0 or 1");
         }
         this.value = value;
     }
 
+    public Bit(Bit other) {
+        value = other.value;
+    }
+
     public Bit add(Bit bit) {
-        return new Bit((byte) ((bit.value + value) % 2));
+        return new Bit((bit.value + value) % 2);
     }
 
     public Bit mult(Bit bit) {
-        return new Bit((byte) (bit.value * value));
+        return new Bit(bit.value * value);
+    }
+
+    public Bit not() {
+        return new Bit((value + 1) % 2);
     }
 
     public static Bit add(Bit one, Bit two) {
-        return new Bit((byte) ((one.value + two.value) % 2));
+        return new Bit((one.value + two.value) % 2);
     }
 
     public static Bit mult(Bit one, Bit two) {
-        return new Bit((byte) (one.value * two.value));
+        return new Bit(one.value * two.value);
     }
 
-    public byte v() {
+    public int v() {
         return value;
     }
 
-    public void v(byte value) {
+    public void v(int value) {
         if (value > 1 || value < 0) {
             throw new IllegalArgumentException("The value must be 0 or 1");
         }
--- src/ReedMuller.java
+++ src/ReedMuller.java
@@ -7,7 +7,29 @@ public class ReedMuller {
 
     public ReedMuller(int r) {
         this.r = r;
+        buildG();
     }
 
-    
+    private void buildG() {
+        int lines = r + 1;
+        int columns = (int) (Math.pow(2, r));
+        Bit temp = new Bit(0);
+        g = new Bit[lines][columns];
+        int alternating = 0;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < columns; j++) {
+                g[i][j] = new Bit(temp);
+                alternating++;
+                if (alternating == (int) Math.pow(2, i)) {
+                    temp = temp.not();
+                    alternating = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < columns; i++) {
+            g[r][i] = new Bit(1);
+        }
+    }
 }
