b8efeac7b2d4e1c254e02a11871c0cd2dfde105d
steven.liatti <steven.liatti@etu.hesge.ch>
2017-06-03 14:11:35 +0200

    Le menu marche bien c'est cool.
    Modifications qui seront validées :
    	modifié :         data/lena_decoded.pgm
    	supprimé :        data/lena_denoised.pgm
    	supprimé :        data/lena_denoised_decoded.pgm
    	nouveau fichier : data/lena_encoded_denoised.pgm
    	nouveau fichier : data/lena_encoded_noised.pgm
    	supprimé :        data/lena_noised.pgm
    	supprimé :        data/mars_007_denoised.pgm
    	supprimé :        data/mars_007_denoised_decoded.pgm
    	renommé :         data/mars_010_denoised_decoded.pgm -> data/mars_decoded.pgm
    	renommé :         data/mars_010_denoised.pgm -> data/mars_denoised.pgm
    	modifié :         src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import images.PGM;
-import reedmuller.Bit;
 import reedmuller.ReedMuller;
 import reedmuller.Word;
 
@@ -8,87 +7,13 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Scanner;
 
-import static images.PGM.*;
-import static reedmuller.Word.*;
+import static images.PGM.read;
+import static images.PGM.write;
+import static reedmuller.Word.bigIntToWord;
 
 public class Main {
 
-    public static void testReader() throws IOException {
-        System.out.println("testReader Lena");
-        PGM pgm = read("data/lena_128x128_64.pgm");
-        System.out.println(pgm);
-
-        write(pgm, "data/lena_copy.pgm");
-        write(pgm.encode(), "data/lena_encoded.pgm");
-
-        PGM pgm2 = read("data/lena_encoded.pgm");
-        write(pgm2.decode(), "data/lena_decoded.pgm");
-        write(pgm2.noise(0.1), "data/lena_noised.pgm");
-
-        PGM pgm3 = read("data/lena_noised.pgm");
-        write(pgm3.denoise(), "data/lena_denoised.pgm");
-        write(pgm3.denoise().decode(), "data/lena_denoised_decoded.pgm");
-    }
-
-    static void testReader2() throws IOException {
-        System.out.println("testReader Mars");
-        PGM pgm4 = read("data/mars-crat.enc.alt_0.07");
-        write(pgm4.denoise(), "data/mars_007_denoised.pgm");
-        write(pgm4.denoise().decode(), "data/mars_007_denoised_decoded.pgm");
-
-        PGM pgm5 = read("data/mars-crat.enc.alt_0.10");
-        write(pgm5.denoise(), "data/mars_010_denoised.pgm");
-        write(pgm5.denoise().decode(), "data/mars_010_denoised_decoded.pgm");
-    }
-
-    static void testFunction() {
-        System.out.println(log2(63));
-        System.out.println(log2(64));
-        BigInteger bg = new BigInteger("41");
-        BigInteger bg2 = new BigInteger("64");
-        System.out.println("bg " + bg.bitLength());
-        System.out.println("bg2 " + bg2.bitLength());
-        System.out.println(bg2.toString(2));
-        Bit b = new Bit('0');
-        Bit b1 = new Bit('1');
-        Bit b2 = new Bit('1');
-        System.out.println("bbbb : " + b + "," + b1 + "," + b2 + " ");
-
-        System.out.println("------------- Word ---------------");
-        Word word = bigIntToWord(bg);
-
-        Word word2 = new ReedMuller(5).encode(word);
-
-        System.out.println(word);
-        System.out.println(wordToBigInt(word));
-
-        System.out.println(word2);
-        System.out.println(wordToBigInt(word2));
-
-
-        System.out.println("Table");
-        int rr = 5;
-        ReedMuller rm = new ReedMuller(rr);
-
-        for (Integer i = 0; i < Math.pow(2, rr + 1); i++) {
-            System.out.print("\ndecimal :\t" + i + "\nbinary  :\t");
-            Word goodSize = bigIntToWord(new BigInteger(i.toString()), rr + 1);
-            System.out.println(goodSize);
-            System.out.print("encoded :\t");
-            Word wordCoded = rm.encode(goodSize);
-            System.out.println(wordCoded);
-            System.out.println("decimal :\t" + wordToBigInt(wordCoded));
-//            System.out.print("decoded :\t");
-//            System.out.println(rm.decode(wordCoded));
-//            System.out.println("decimal :\t" + wordToBigInt(goodSize));
-        }
-
-    }
-
     public static void main(String[] args) throws IOException {
-//        System.out.println(log2(63));
-        testReader2();
-
         // permet de prendre les entrées pour le menu
         // soit du clavier, d'un fichier ou de la ligne de commande
         Scanner in;
@@ -146,13 +71,13 @@ public class Main {
                         current = rm.encode(intToWord);
                         break;
                     case 2:
-                        current = rm.decode(intToWord);
+                        current = rm.decode(current);
                         break;
                     case 3:
-                        current = rm.noise(intToWord, seuil);
+                        current = rm.noise(current, seuil);
                         break;
                     case 4:
-                        current = rm.semiExhaustiveSearch(intToWord);
+                        current = rm.semiExhaustiveSearch(current);
                         break;
                     case 5:
                         System.err.println("\nEntrer un mot (en décimal)");
@@ -169,35 +94,33 @@ public class Main {
                 choix = in.nextInt();
             } while (choix != 0);
         } else if (mode == 2) {
+            choix = 5;
+            String fileName = null;
+            PGM pgm = null;
             do {
-                String fileName;
                 switch (choix) {
                     case 1:
-                        // vos opérations pour l'encodage de l'image courante,
-                        // ne rien afficher sur la sortie standard
+                        pgm = pgm.encode();
                         break;
                     case 2:
-                        // vos opérations pour le décodage de l'image courante,
-                        // ne rien afficher sur la sortie standard
+                        pgm = pgm.decode();
                         break;
                     case 3:
-                        // vos opérations pour le bruitage de l'image courante,
-                        // ne rien afficher sur la sortie standard
+                        pgm = pgm.noise(seuil);
                         break;
                     case 4:
-                        // vos opérations pour le débruitage de l'image courante,
-                        // ne rien afficher sur la sortie standard
+                        pgm = pgm.denoise();
                         break;
                     case 5:
-                        System.err.println("Nom du fichier de l'image à charger (format png):");
+                        System.err.println("Nom du fichier de l'image à charger (format pgm):");
                         fileName = in.next();
-                        // lire le fichier contenant l'image images
+                        pgm = read(fileName);
                         break;
                 }
                 if (choix != 5) {
-                    System.err.println("Nom du fichier où sauver l'image courante (format png):");
+                    System.err.println("Nom du fichier où sauver l'image courante (format pgm):");
                     fileName = in.next();
-                    // sauver l'image courante au format images
+                    write(pgm, fileName);
                 }
                 System.err.println(menu);
                 choix = in.nextInt();
